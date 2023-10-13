b71d51bc506ba6df1a999444d9c9c5641fb1f7e7
saulsistemas <santamariaramos18@hotmail.com>
2023-10-13 08:58:53 -0500

    JS - REPASO ASYNC AWAIT

--- 01CONCEPTOSJS/index.js
+++ 01CONCEPTOSJS/index.js
@@ -879,3 +879,41 @@ getEmpleadoBI2(idl)
         console.log("El empleado: "+nombrePromesa+" tiene el salario de : "+ sala);
     })
     .catch(function(err){console.log(err);})
+
+console.log("==================== 15 ASYNC AWAIT==================== ");
+
+const getEmpleadoBI3= function(id){
+    //FUNCION DE FLECHA Y ENVIAR UN SOLO ELEMENTO
+    return new Promise(function(resuelto, rechazado){
+        const empleados = empleadosBD.find(elemento => elemento.id === id)?.nombre;
+        (empleados)
+        ? resuelto(empleados)
+        : rechazado('no exite el usuario de la promesa '+ id)
+    })
+} 
+const getSalarioBD3= function(id){
+    //FUNCION DE FLECHA Y ENVIAR UN SOLO ELEMENTO
+    return new Promise(function(resuelto, rechazado){
+        const salario = salarioBD.find(elemento => elemento.id === id)?.salario;
+        (salario)
+        ? resuelto(salario)
+        : rechazado('no exite el salario de la promesa '+ id)
+    })
+} 
+//ASYNC TRANSFORMA UNA FUNCION AINCRONA PARA REGRESAR UNA PROMESA
+//AWAIT 
+const getInfoUsuarios = async function(id) {
+    try {
+        const empleado = await getEmpleadoBI3(id);
+        const salario = await getSalarioBD3(id);
+        return "El empleado es: "+empleado+ " y su salario: "+salario ;
+    } catch (error) {
+        return error;
+    }
+}
+
+getInfoUsuarios(3)
+    .then(function(empleado){
+        console.log("WAIT "+empleado);
+    })
+    .catch(err => console.log(err))//solo se utiliza un catch para todo los errores
