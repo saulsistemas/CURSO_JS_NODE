00d437acd5aa1cc6b2626babf705dbd2957b25ad
saulsistemas <santamariaramos18@hotmail.com>
2023-10-14 21:11:44 -0500

    handlebars

--- 04CONCEPTOSWEB/app.js
+++ 04CONCEPTOSWEB/app.js
@@ -1,11 +1,19 @@
 const express = require('express');
+const path = require('path');
+
 const app = express()
 const port = 8080;
+//TODO: require('hbs)
+const viewsPath = path.join(__dirname, '/public/views');
+app.set('views', viewsPath);
+app.set('view engine', 'hbs');
 
 //Servir contenido estatico
 app.use(express.static('public'));
 
-
+app.get('/',function(request,response){
+    response.render('home')
+})
 app.get('/generic', function (req, res) {
     res.sendFile(__dirname+'/public/generic.html')
 })
