4a973f47fbe717546f0a90f28accfc47763181b0
saulsistemas <santamariaramos18@hotmail.com>
2023-10-14 20:23:52 -0500

    agregando paginas web y configurando contenido estatico

--- 04CONCEPTOSWEB/app.js
+++ 04CONCEPTOSWEB/app.js
@@ -2,16 +2,16 @@ const express = require('express');
 const app = express()
 const port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+//Servir contenido estatico
+app.use(express.static('public'));
+
 
 app.get('/hola-mundo', function (req, res) {
     res.send('Hola mundo en su respectiva ruta')
 })
 
 app.get('*', function (req, res) {
-    res.send('404 | Page not found')
+    res.sendFile(__dirname+'/public/404.html')
 })
 app.listen(port,function(){
     console.log("el servidor esta funcionando en el server ",port);
