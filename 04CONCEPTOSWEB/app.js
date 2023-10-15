d0bd9c8028398ade8adff32dc9786c8600424670
saulsistemas <santamariaramos18@hotmail.com>
2023-10-14 20:45:00 -0500

    subiendo archivos html a la carpeta publica y configurar nueva ruta

--- 04CONCEPTOSWEB/app.js
+++ 04CONCEPTOSWEB/app.js
@@ -6,10 +6,12 @@ const port = 8080;
 app.use(express.static('public'));
 
 
-app.get('/hola-mundo', function (req, res) {
-    res.send('Hola mundo en su respectiva ruta')
+app.get('/generic', function (req, res) {
+    res.sendFile(__dirname+'/public/generic.html')
 })
-
+app.get('/elements', function (req, res) {
+    res.sendFile(__dirname+'/public/elements.html')
+})
 app.get('*', function (req, res) {
     res.sendFile(__dirname+'/public/404.html')
 })
