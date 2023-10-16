3bf2d0b37ea6386ad820d5844f8448056bc4545e
saulsistemas <santamariaramos18@hotmail.com>
2023-10-16 13:22:21 -0500

    validando correo

--- 05RESTSERVER/routes/user.router.js
+++ 05RESTSERVER/routes/user.router.js
@@ -1,11 +1,14 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { usuarioGet, usuarioPut, usuarioPost, usuarioDelete, usuarioPatch } = require('../controllers/userController');
 //const router = require('express').Router();
 const router = Router();
 
 router.get('/',usuarioGet)
 router.put('/:id',usuarioPut)
-router.post('/',usuarioPost)
+router.post('/',[
+    check('correo','el correo no es valido').isEmail(),
+],usuarioPost)
 router.delete('/',usuarioDelete)
 router.patch('/',usuarioPatch)
 
