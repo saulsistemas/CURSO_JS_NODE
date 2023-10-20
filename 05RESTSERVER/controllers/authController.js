0be7ef657ff73094388132b4dff88e9adebeafad
saulsistemas <santamariaramos18@hotmail.com>
2023-10-20 10:51:23 -0500

    validaciones de logeo de usuario

--- 05RESTSERVER/controllers/authController.js
+++ 05RESTSERVER/controllers/authController.js
@@ -1,9 +1,43 @@
-const { response,request } = require("express")
+const { response,request } = require("express");
+const Usuario = require("../models/user");
+const bcryptjs = require('bcryptjs')
 
-const login = (req,res=response)=>{
-    res.json({
-        msg:'todo ok'
-    })
+const login = async(req,res=response)=>{
+    const {correo,password}=req.body;
+    try {
+    //verificar si el correo existe
+    const usuario = await Usuario.findOne({correo})
+    if (!usuario) {
+        return res.status(400).json({
+            msg:'Usuario / password no son correctos -correo'
+        })
+    }
+    //si el usuario esta activo
+    if (!usuario.estado) {
+        return res.status(400).json({
+            msg:'Usuario / password no son correctos -Estado: False'
+        })
+    }
+    //verificar la contraseña
+    const validarPassword = bcryptjs.compareSync(password,usuario.password)
+    if (!validarPassword) {
+        return res.status(400).json({
+            msg:'Usuario / password no son correctos -Password'
+        })
+    }
+    //generar el JWT
+
+        res.json({
+            msg:'todo ok',
+            //correo,password
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg:'Hable con el administrador'
+        })
+    }
+
 }
 
 module.exports ={
