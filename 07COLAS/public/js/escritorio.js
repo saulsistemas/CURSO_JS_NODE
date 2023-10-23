4eac2f448dba0924bbcdb26833d5032d341bd020
saulsistemas <santamariaramos18@hotmail.com>
2023-10-23 18:32:27 -0500

    Mostrar ticket en tiempo real

--- 07COLAS/public/js/escritorio.js
+++ 07COLAS/public/js/escritorio.js
@@ -2,6 +2,7 @@ const lblEscritorio  = document.querySelector('h1')
 const btnAtender     = document.querySelector('button')
 const lblTicket     = document.querySelector('small')
 const divAlerta     = document.querySelector('.alert')
+const lblPendientes     = document.querySelector('#lblPendientes')
 
 const searchParams = new URLSearchParams(window.location.search)
 
@@ -31,8 +32,14 @@ socket.on('disconnect', () => {
     btnAtender.disabled = true;
 });
 
-socket.on('ultimo-ticket',(ultimo)=>{
-    //lblNuevoTicket.innerText = "Ticket "+ ultimo
+socket.on('tickets-pendientes',(pendientes)=>{
+    if (pendientes===0) {
+        lblPendientes.style.display = 'none'
+    }else{
+        lblPendientes.style.display =''
+        lblPendientes.innerText = pendientes;
+        console.log(pendientes);
+    }
 })
 
 
--- 07COLAS/sockets/controller.js
+++ 07COLAS/sockets/controller.js
@@ -6,14 +6,20 @@ const ticketControl = new TicketControl();
 const socketController = (socket) => { 
     console.log('socket io conectado', socket.id);   
     
+    //cuando un cliente se conecta
     socket.emit('ultimo-ticket',ticketControl.ultimo)
     socket.emit('estado-actual',ticketControl.ultimo4)
+    socket.emit('tickets-pendientes',ticketControl.tickets.length)
+
+   
+
 
     socket.on('siguiente-ticket',(payload,callback)=>{
         //console.log(payload);
         
         const siguiente = ticketControl.siguiente();
         callback(siguiente);
+        socket.broadcast.emit('tickets-pendientes',ticketControl.tickets.length)
 
         //TODO: Notofocar que hay un ticket pendiente
     })
@@ -29,6 +35,8 @@ const socketController = (socket) => {
         }
         const ticket = ticketControl.atenderTicket(escritorio);
         socket.broadcast.emit('estado-actual',ticketControl.ultimo4)
+        socket.emit('tickets-pendientes',ticketControl.tickets.length)
+        socket.broadcast.emit('tickets-pendientes',ticketControl.tickets.length)
         if (!ticket) {
             callback({
                 ok:false,
